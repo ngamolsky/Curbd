fff6f46b979dc0d35051ea9578528b4a34567465
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2019-06-29 17:08:52 -0700

    create item model and add gitignore, small refactor to remove extra directory

--- curbd/curbd_app/models.py
+++ curbd/curbd_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+    create_date = models.DateTimeField('date create')
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
