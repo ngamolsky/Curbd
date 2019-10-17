302d9b0e3c25e5e65cfa612ba866b2e8e827c00d
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2019-10-16 17:55:19 -0700

    add items model to admin and change up urls a little

--- curbd/curbd_app/urls.py
+++ curbd/curbd_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.login, name='login'),
+    path('admin/', admin.site.urls),
 ]
